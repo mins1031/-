7207c27ba0ce951cc9e3d9bb2e2345578e477b59
mins1031 <urisegea@naver.com>
2021-11-28 23:36:37 +0900

    feat: 젠킨스 삽질;

--- src/main/java/com/example/nonstoppractice/contorller/MainController.java
+++ src/main/java/com/example/nonstoppractice/contorller/MainController.java
@@ -1,15 +1,28 @@
 package com.example.nonstoppractice.contorller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
+@AllArgsConstructor
 public class MainController {
+    private final Environment environment;
 
     @GetMapping()
     public ResponseEntity getMain() {
         String name = "Main";
         return ResponseEntity.ok().body(name);
+    }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(environment.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
