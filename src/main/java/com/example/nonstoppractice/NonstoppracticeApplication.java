6cb9ec9b9e343fab93a0354b793a9c582e85bdb9
mins1031 <urisegea@naver.com>
2021-12-22 14:02:11 +0900

    style: 젠킨스 테스트3

--- src/main/java/com/example/nonstoppractice/NonstoppracticeApplication.java
+++ src/main/java/com/example/nonstoppractice/NonstoppracticeApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.AbstractEnvironment;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
@@ -14,16 +15,9 @@ import java.util.Arrays;
 @SpringBootApplication
 public class NonstoppracticeApplication {
 
-//    @Autowired
-//    private static Environment environment;
-//
-//    private static String localYmlPath = "/Users/macbookair";
-//    private static String liveYmlPath = "/home/ubuntu";
-
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.yml,"
-//            + assortYmlFilePath()
-            + "app/config/real-application.yml";
+            + "../../app/config/real-application.yml";
 
     public static void main(String[] args) {
         new SpringApplicationBuilder(NonstoppracticeApplication.class)
@@ -31,13 +25,4 @@ public class NonstoppracticeApplication {
                 .run(args);
 //        SpringApplication.run(NonstoppracticeApplication.class);
     }
-
-//    public static String assortYmlFilePath() {
-//        if (Arrays.stream(environment.getActiveProfiles()).findFirst().orElse("").equals("local")) {
-//            return localYmlPath;
-//        }
-//
-//        return liveYmlPath;
-//    }
-
 }
